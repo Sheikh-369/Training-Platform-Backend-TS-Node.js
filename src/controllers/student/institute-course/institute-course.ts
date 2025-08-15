2ab674f7e1f6ea15283c2813c31613b1d4f9e7f7
Zunaid Sheikh <lamhaa786@gmail.com>
2025-08-15 15:21:10 +0545

    fetching-institutes-in-student-interface

--- src/controllers/student/institute-course/institute-course.ts
+++ src/controllers/student/institute-course/institute-course.ts
@@ -3,12 +3,31 @@ import sequelize from "../../../database/connection";
 import { QueryTypes } from "sequelize";
 
 const fetchInstitutes=async(req:Request,res:Response)=>{
+    //logic to get all the institutes
     const tables=await sequelize.query(`SHOW TABLES LIKE "institute_%"`,{
         type:QueryTypes.SHOWTABLES
     })
+
+    let allData=[]//data collection
+
+    //logic for getting all tables 
+    for(let table of tables){
+        console.log(table)
+
+        //logic to extract institute number
+        const instituteNumber=table.split("_")[1]
+
+        //logic to extract name,address,phone,etc from each table
+        const [data]=await sequelize.query(`SELECT instituteName,instituteAddress,institutePhoneNumber FROM ${table}`,{
+            type:QueryTypes.SELECT
+        })
+        
+        //pushing filtered data in data collection
+        allData.push({instituteNumber:instituteNumber,...data})
+    }
     res.status(200).json({
         message:"All Institutes Fetched Successfully!",
-        data:tables
+        data:allData
     })
     console.log(tables)
 }
